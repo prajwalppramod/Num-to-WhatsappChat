800b4a123ef078d76818069b78d6aa314521a8af
Prajwal P Pramod <98303757+prajwalppramod@users.noreply.github.com>
2023-08-03 21:25:36 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
 document.addEventListener("DOMContentLoaded", function() {
     const startChatButton = document.getElementById("startChat");
+    const countryDropdown = document.getElementById("country");
+  
+    // Load country codes from JSON file
+    fetch("countryCodes.json")
+      .then(response => response.json())
+      .then(data => {
+        data.forEach(country => {
+          const option = document.createElement("option");
+          option.value = country.dialCode;
+          option.textContent = `${country.name} (${country.dialCode})`;
+          countryDropdown.appendChild(option);
+        });
+  
+        // Set default option to India
+        const defaultCountryCode = "+91";
+        countryDropdown.value = defaultCountryCode;
+      })
+      .catch(error => console.error("Error loading country codes:", error));
   
     startChatButton.addEventListener("click", function() {
-      const countryDropdown = document.getElementById("country");
       const phoneNumberInput = document.getElementById("phoneNumber");
       const countryCode = countryDropdown.value;
       const phoneNumber = phoneNumberInput.value;
   
-      if (phoneNumber) {
+      if (phoneNumber && countryCode) {
         const whatsappLink = `https://wa.me/${countryCode}${phoneNumber}`;
         window.location.href = whatsappLink;
       }
